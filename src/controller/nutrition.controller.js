2e72975a96259160b949edd656b3728d03bc1d06
Daniel Guzmán <61705708+id-guzman01@users.noreply.github.com>
2022-09-07 22:30:45 -0500

    Update nutrition.controller.js

--- src/controller/nutrition.controller.js
+++ src/controller/nutrition.controller.js
@@ -8,7 +8,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
-    password: '1234',
+    password: '28482B4D62516554',
     database: 'gimnasio',
     port: 5432
 });
